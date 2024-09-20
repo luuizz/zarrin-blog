22d7f67262479568973f3f8de530476486e0ec41
luuizz <luu.izz@hotmail.com>
2024-09-20 01:26:19 -0300

    feat: extendido container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      maxWidth: {
+        'container': '1236px'
+      }
     },
   },
   plugins: [],
