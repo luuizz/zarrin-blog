43b82d442dca6e7fefc80a28ec093c2dd25794b7
luuizz <luu.izz@hotmail.com>
2024-09-20 01:33:47 -0300

    fix: ajuste na tipagem

--- src/components/Container/container.tsx
+++ src/components/Container/container.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { twMerge } from 'tailwind-merge';
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ContainerProps {
-  children: React.ReactNode,
+  children?: React.ReactNode;
   className?: string;
 }
 
 export default function Container({ children, className }: ContainerProps) {
   return (
-    <div className={twMerge("mx-auto px-3 w-full max-w-container", className)}>{children}</div>
-  )
+    <div className={twMerge("mx-auto w-full max-w-container px-3", className)}>
+      {children}
+    </div>
+  );
 }
