aa4001d32ce475eb00de4dab89918027209703ec
luuizz <luu.izz@hotmail.com>
2024-09-20 23:12:23 -0300

    feat: criado footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../Container/container";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <p>Footer</p>
+      </Container>
+    </footer>
+  );
+}
