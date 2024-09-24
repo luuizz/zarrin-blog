4d2fb5fbdee49099ff2c7913a07b7982974b1916
luuizz <luu.izz@hotmail.com>
2024-09-23 23:06:06 -0300

    feat: adicionado dados para posts destacados

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,13 @@
+import imgPost01 from "@/assets/thumb-1.png";
+import imgPost02 from "@/assets/thumb-2.png";
+import imgPost03 from "@/assets/thumb-3.png";
+import imgPost04 from "@/assets/thumb-4.png";
+import imgPost05 from "@/assets/thumb-5.png";
+import imgPost06 from "@/assets/thumb-6.png";
+import imgPost07 from "@/assets/thumb-7.png";
+import imgPost08 from "@/assets/thumb-8.png";
+import imgPost09 from "@/assets/thumb-9.png";
+
 export const linksMenu = [
   {
     label: "Blog",
@@ -6,5 +16,32 @@ export const linksMenu = [
   {
     label: "Sobre nós",
     url: "/sobre-nos",
+  },
+];
+
+export const dataPostRecent = [
+  {
+    category: "Travel",
+    dataPublicacao: "13 March 2023",
+    tituloPost: "8 Rules of Travelling In Sea You Need To Know",
+    descricaoCurta:
+      "Travelling in sea has many advantages. Some of the advantages of transporting goods by sea include: you can ship large volumes at costs ",
+    bannerPost: imgPost01,
+  },
+  {
+    category: "Development",
+    dataPublicacao: "13 March 2023",
+    tituloPost: "How to build strong portfolio and get a Job in UI/UX",
+    descricaoCurta:
+      "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from",
+    bannerPost: imgPost02,
+  },
+  {
+    category: "Sports",
+    dataPublicacao: "13 March 2023",
+    tituloPost: "How to Be a Professional Footballer in 2023",
+    descricaoCurta:
+      "Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. survival strategies to ensure proactive",
+    bannerPost: imgPost03,
   },
 ];
